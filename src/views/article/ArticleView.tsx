311b7ce7b9f281d39e0957afd232c7714ffed069
Micah <ranaivo.it@gmail.com>
2023-09-06 16:49:34 +0300

    Modification client, fournisseur, article view

--- src/views/article/ArticleView.tsx
+++ src/views/article/ArticleView.tsx
@@ -1,11 +1,359 @@
-import React from 'react'
+import { useState } from 'react'
+import { Badge, Button, ButtonGroup, Card, Col, Form, Modal, Row, Table } from 'react-bootstrap'
+import { AiOutlinePlus, AiOutlineEye, AiOutlineEdit, AiOutlineDelete, AiOutlineClose, AiOutlineSave, AiOutlinePrinter, AiOutlineCheck } from "react-icons/ai"
 
 type Props = {}
 
-function ArticleView({}: Props) {
-  return (
-    <div>ArticleView</div>
-  )
+export default function ArticleView({ }: Props) {
+	const [selectedRow, setSelectedRow] = useState(-1)
+
+	const handleRowSelect = (e: any, row: any) => {
+		if (e.target.checked) {
+			setSelectedRow(row)
+		} else {
+			setSelectedRow(-1)
+		}
+	}
+
+
+	return (
+		<>
+			<Card>
+				<Card.Header>
+					<div className="d-flex align-items-center justify-content-between mb-0">
+						<h2 className="h2 mb-0 ">Article</h2>
+						<ButtonGroup size='sm'>
+							<AddModal />
+							<ShowModal />
+							<EditModal />
+							<DeleteModal />
+						</ButtonGroup>
+
+					</div>
+				</Card.Header>
+
+				<Card.Body>
+					<Table responsive>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>ID</th>
+								<th>Désignation</th>
+								<th>Référence</th>
+								<th>Unité</th>
+								<th>P.V</th>
+								<th>Statut</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td key={1} className="align-middle">
+									<input
+										type="checkbox"
+										checked={(selectedRow == 1) ? true : false}
+										onChange={(e) => handleRowSelect(e, 1)}
+									/>
+								</td>
+								<td className="align-middle">1</td>
+								<td className="align-middle">Article 1</td>
+								<td className="align-middle">ASDSF</td>
+								<td className="align-middle">Piece</td>
+								<td className="align-middle">0.0</td>
+								<td className="align-middle"><Badge bg="success">Actif</Badge></td>
+
+							</tr>
+
+							<tr>
+								<td key={2} className="align-middle">
+									<input
+										type="checkbox"
+										checked={(selectedRow == 2) ? true : false}
+										onChange={(e) => handleRowSelect(e, 2)}
+									/>
+								</td>
+								<td className="align-middle">1</td>
+								<td className="align-middle">Article 1</td>
+								<td className="align-middle">ASDSF</td>
+								<td className="align-middle">Piece</td>
+								<td className="align-middle">0.0</td>
+								<td className="align-middle"><Badge bg="success">Actif</Badge></td>
+
+							</tr>
+
+
+						</tbody>
+					</Table>
+				</Card.Body>
+			</Card>
+		</>
+
+	)
 }
 
-export default ArticleView
+function AddModal() {
+	const [show, setShow] = useState(false)
+
+	const handleClose = () => setShow(false)
+	const handleShow = () => setShow(true)
+
+	return (
+		<>
+			<Button variant='success' onClick={handleShow} >
+				<AiOutlinePlus />
+			</Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Nouveau Article</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Désignation</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Référence</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Catégorie</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Unité de vente</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+						</Row>
+
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Prix d'Achat</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Prix de Vente</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+
+						<Form.Group as={Col} controlId="" className='mb-3'>
+							<Form.Label>Stock minimum</Form.Label>
+							<Form.Control type="text" placeholder="" />
+						</Form.Group>
+
+						<Form.Group className="mb-3" id="formGridCheckbox">
+							<Form.Check type="checkbox" label="Actif" />
+						</Form.Group>
+
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlineSave />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	)
+}
+
+function EditModal() {
+	const [show, setShow] = useState(false)
+
+	const handleClose = () => setShow(false)
+	const handleShow = () => setShow(true)
+
+	return (
+		<>
+
+			<Button variant="warning" onClick={handleShow}><AiOutlineEdit /></Button>
+
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Modification Article</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className="mb-3" controlId="formGridId">
+							<Form.Label>ID</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Désignation</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Référence</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Catégorie</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Unité de vente</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+						</Row>
+
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Prix d'Achat</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="">
+								<Form.Label>Prix de Vente</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+
+						<Form.Group as={Col} controlId="" className='mb-3'>
+							<Form.Label>Stock minimum</Form.Label>
+							<Form.Control type="text" placeholder="" />
+						</Form.Group>
+
+						<Form.Group className="mb-3" id="formGridCheckbox">
+							<Form.Check type="checkbox" label="Actif" />
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlineSave />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	)
+}
+
+function ShowModal() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button variant="primary" onClick={handleShow}><AiOutlineEye /></Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Article</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+
+					<Table>
+						<tr>
+							<td>ID</td>
+							<td>CL0001</td>
+						</tr>
+						<tr>
+							<td>Désignation</td>
+							<td>Article 1</td>
+						</tr>
+						<tr>
+							<td>Référence</td>
+							<td>AAAA1</td>
+						</tr>
+						<tr>
+							<td>Catégorie</td>
+							<td>+</td>
+						</tr>
+						<tr>
+							<td>Unité de vent</td>
+							<td>Piece</td>
+						</tr>
+						<tr>
+							<td>Prix d'achat</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>Prix de vente</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>Stock minimum</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>Statut</td>
+							<td>Actif</td>
+						</tr>
+					</Table>
+
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlinePrinter />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+}
+
+function DeleteModal() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button variant="danger" onClick={handleShow}><AiOutlineDelete /></Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Suppression Article</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>Voulez vous vraiment supprimer le client : 'CL0001' 'SARL Anonyme' ?</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-danger" onClick={handleClose}>
+						<AiOutlineCheck />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+}
--- src/views/customer/CustomerView.tsx
+++ src/views/customer/CustomerView.tsx
@@ -1,9 +1,352 @@
-import React from 'react'
+import { useState } from 'react'
+import { Badge, Button, ButtonGroup, Card, Col, Form, Modal, Row, Table } from 'react-bootstrap'
+import { AiOutlinePlus, AiOutlineEye, AiOutlineEdit, AiOutlineDelete, AiOutlineClose, AiOutlineSave, AiOutlinePrinter, AiOutlineCheck } from "react-icons/ai"
 
 type Props = {}
 
-export default function CustomerView({}: Props) {
-  return (
-    <div>CustomerView</div>
-  )
+export default function CustomerView({ }: Props) {
+	const [selectedRow, setSelectedRow] = useState(-1)
+
+	const handleRowSelect = (e: any, row: any) => {
+		if (e.target.checked) {
+			setSelectedRow(row)
+		} else {
+			setSelectedRow(-1)
+		}
+	}
+
+
+	return (
+		<>
+			<Card>
+				<Card.Header>
+					<div className="d-flex align-items-center justify-content-between mb-0">
+						<h2 className="h2 mb-0 ">Client</h2>
+						<ButtonGroup size='sm'>
+							<AddModal />
+							<ShowModal />
+							<EditModal />
+							<DeleteModal />
+						</ButtonGroup>
+						
+					</div>
+				</Card.Header>
+
+				<Card.Body>
+					<Table responsive>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>ID</th>
+								<th>Intitulé</th>
+								<th>Téléphone</th>
+								<th>Pays</th>
+								<th>Ville</th>
+								<th>Statut</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td key={1} className="align-middle">
+									<input
+										type="checkbox"
+										checked={(selectedRow == 1) ? true : false}
+										onChange={(e) => handleRowSelect(e, 1)}
+									/>
+								</td>
+								<td className="align-middle">1</td>
+								<td className="align-middle">Mark</td>
+								<td className="align-middle">+261320233202</td>
+								<td className="align-middle">Madagascar</td>
+								<td className="align-middle">Antananarivo</td>
+								<td className="align-middle"><Badge bg="success">Actif</Badge></td>
+								
+							</tr>
+
+							<tr>
+								<td key={2} className="align-middle">
+									<input
+										type="checkbox"
+										checked={(selectedRow == 2) ? true : false}
+										onChange={(e) => handleRowSelect(e, 2)}
+									/>
+								</td>
+								<td className="align-middle">1</td>
+								<td className="align-middle">Mark</td>
+								<td className="align-middle">+261320233202</td>
+								<td className="align-middle">Madagascar</td>
+								<td className="align-middle">Antananarivo</td>
+								<td className="align-middle"><Badge bg="success">Actif</Badge></td>
+								
+							</tr>
+
+
+						</tbody>
+					</Table>
+				</Card.Body>
+			</Card>
+		</>
+
+	)
+}
+
+function AddModal() {
+	const [show, setShow] = useState(false)
+
+	const handleClose = () => setShow(false)
+	const handleShow = () => setShow(true)
+
+	return (
+		<>
+			<Button variant='success' onClick={handleShow} >
+				<AiOutlinePlus />
+			</Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Nouveau Client</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className="mb-3" controlId="formGridIntitule">
+							<Form.Label>Intitulé</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="formGridEmail">
+								<Form.Label>Email</Form.Label>
+								<Form.Control type="email" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="formGridTel">
+								<Form.Label>Téléphone</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+
+						<Form.Group className="mb-3" controlId="formGridAddress">
+							<Form.Label>Adresse</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="formGridZip">
+								<Form.Label>Postal</Form.Label>
+								<Form.Control />
+							</Form.Group>
+							<Form.Group as={Col} controlId="formGridCity">
+								<Form.Label>Ville</Form.Label>
+								<Form.Control />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="formGridState">
+								<Form.Label>Pays</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+
+
+						</Row>
+
+						<Form.Group className="mb-3" id="formGridCheckbox">
+							<Form.Check type="checkbox" label="Actif" />
+						</Form.Group>
+
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlineSave />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	)
+}
+
+function EditModal() {
+	const [show, setShow] = useState(false)
+
+	const handleClose = () => setShow(false)
+	const handleShow = () => setShow(true)
+
+	return (
+		<>
+
+			<Button variant="warning"  onClick={handleShow}><AiOutlineEdit /></Button>
+
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Modification Client</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className="mb-3" controlId="formGridId">
+							<Form.Label>ID</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formGridIntitule">
+							<Form.Label>Intitulé</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="formGridEmail">
+								<Form.Label>Email</Form.Label>
+								<Form.Control type="email" placeholder="" />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="formGridTel">
+								<Form.Label>Téléphone</Form.Label>
+								<Form.Control type="text" placeholder="" />
+							</Form.Group>
+						</Row>
+
+						<Form.Group className="mb-3" controlId="formGridAddress">
+							<Form.Label>Adresse</Form.Label>
+							<Form.Control placeholder="" />
+						</Form.Group>
+
+						<Row className="mb-3">
+							<Form.Group as={Col} controlId="formGridZip">
+								<Form.Label>Postal</Form.Label>
+								<Form.Control />
+							</Form.Group>
+							<Form.Group as={Col} controlId="formGridCity">
+								<Form.Label>Ville</Form.Label>
+								<Form.Control />
+							</Form.Group>
+
+							<Form.Group as={Col} controlId="formGridState">
+								<Form.Label>Pays</Form.Label>
+								<Form.Select defaultValue="Choose...">
+									<option>Choose...</option>
+									<option>...</option>
+								</Form.Select>
+							</Form.Group>
+
+
+						</Row>
+
+						<Form.Group className="mb-3" id="formGridCheckbox">
+							<Form.Check type="checkbox" label="Actif" />
+						</Form.Group>
+
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlineSave />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	)
+}
+
+function ShowModal() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button variant="primary" onClick={handleShow}><AiOutlineEye /></Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Client</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+
+					<Table>
+						<tr>
+							<td>ID</td>
+							<td>CL0001</td>
+						</tr>
+						<tr>
+							<td>Intitulé</td>
+							<td>SARL Anonyme</td>
+						</tr>
+						<tr>
+							<td>Email</td>
+							<td>aaa@a.a</td>
+						</tr>
+						<tr>
+							<td>Téléphone</td>
+							<td>+261320323203</td>
+						</tr>
+						<tr>
+							<td>Adresse</td>
+							<td>Lot xxxx</td>
+						</tr>
+						<tr>
+							<td>Postal</td>
+							<td>001x</td>
+						</tr>
+						<tr>
+							<td>Ville</td>
+							<td>Tana</td>
+						</tr>
+						<tr>
+							<td>Pays</td>
+							<td>Mada</td>
+						</tr>
+						<tr>
+							<td>Statut</td>
+							<td>Actif</td>
+						</tr>
+					</Table>
+
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-primary" onClick={handleClose}>
+						<AiOutlinePrinter />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+}
+
+function DeleteModal() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button variant="danger" onClick={handleShow}><AiOutlineDelete /></Button>
+
+			<Modal show={show} onHide={handleClose} size='lg'>
+				<Modal.Header closeButton>
+					<Modal.Title>Suppression Client</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>Voulez vous vraiment supprimer le client : 'CL0001' 'SARL Anonyme' ?</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						<AiOutlineClose />
+					</Button>
+					<Button variant="outline-danger" onClick={handleClose}>
+						<AiOutlineCheck />
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
 }
